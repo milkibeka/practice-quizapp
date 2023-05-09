813d55412d03f0b97e91511c8d9a069106f67247
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-05-09 01:50:26 +0100

    starter

---
+++ assets/js/logic.js
@@ -0,0 +1,148 @@
+import { questions } from 'questions.js';
+const startBtn = document.getElementById("start");
+const questionContainer = document.getElementById("questions");
+const questionEl = document.getElementById("question");
+const answerBtnsEl = document.getElementById("answer-buttons");
+const scoreEl = document.getElementById("score");
+const timerEl = document.getElementById("timer");
+const feedbackEl = document.getElementById("feedback");
+const initialsEl = document.getElementById("initials");
+const submitBtn = document.getElementById("submit");
+const highscoresLink = document.getElementById("highscores-link");
+
+let shuffledQuestions, currentQuestionIndex;
+let score = 0;
+let secondsLeft = 60;
+let timerInterval;
+
+startBtn.addEventListener("click", startQuiz);
+submitBtn.addEventListener("click", saveScore);
+highscoresLink.addEventListener("click", showHighscores);
+
+function startQuiz() {
+  score = 0;
+  secondsLeft = 60;
+  startBtn.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainer.classList.remove("hide");
+  setNextQuestion();
+  startTimer();
+}
+
+function startTimer() {
+  let timerInterval = setInterval(function () {
+    secondsLeft--;
+    timerEl.textContent = "Time left: " + secondsLeft;
+
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    }
+  }, 1000);
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerBtnsEl.appendChild(button);
+  });
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    feedbackEl.innerText = "";
+    if (answerBtnsEl !== null) {
+      while (answerBtnsEl.firstChild) {
+        answerBtnsEl.removeChild(answerBtnsEl.firstChild);
+      }
+    }
+  }
+  
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const correct = selectedBtn.dataset.correct;
+  setStatusClass(document.body, correct);
+  if (correct) {
+    score += 10;
+  } else {
+    secondsLeft -= 10;
+  }
+  feedbackEl.innerText = correct ? "Correct!" : "Wrong!";
+  scoreEl.innerText = "Score: " + score;
+  Array.from(answerBtnsEl.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+    button.disabled = true;
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    setTimeout(() => {
+      currentQuestionIndex++;
+      setNextQuestion();
+    }, 1000);
+  } else {
+    setTimeout(() => {
+      endQuiz();
+    }, 1000);
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  questionContainer.classList.add("hide");
+  initialsEl.classList.remove("hide");
+}
+
+function saveScore() {
+  const initials = initialsEl.value.toUpperCase();
+  if (initials !== "") {
+    const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+    highscores.push({ initials, score: score });
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    window.location.assign("highscores.html");
+  }
+}
+function showHighscores() {
+  const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+  // Sort highscores in descending order
+  highscores.sort((a, b) => b.score - a.score);
+
+  const highscoresList = document.getElementById("highscores");
+
+  // Clear highscores list
+  highscoresList.innerHTML = "";
+
+  // Display each highscore in the list
+  highscores.forEach((highscore) => {
+    const li = document.createElement("li");
+    li.textContent = `${highscore.initials}: ${highscore.score}`;
+    highscoresList.appendChild(li);
+  });
+}
---
+++ assets/js/questions.js
@@ -0,0 +1,50 @@
+var questions = [  {    question: "Which of the following is NOT a primitive data type in JavaScript?",    choices: ["Number", "Boolean", "Undefined", "String", "Array"],
+    answer: "Array"
+  },
+  {
+    question: "What does the 'this' keyword refer to in JavaScript?",
+    choices: ["The function itself", "The object that owns the function", "The global object", "The parent object"],
+    answer: "The object that owns the function"
+  },
+  {
+    question: "What does the acronym 'JSON' stand for?",
+    choices: ["JavaScript Object Notation", "JavaScript Online Network", "JavaScript Operations Navigation", "JavaScript Open Network"],
+    answer: "JavaScript Object Notation"
+  },
+  {
+    question: "What is the result of the expression '3' + 2 in JavaScript?",
+    choices: ["5", "32", "NaN", "undefined"],
+    answer: "32"
+  },
+  {
+    question: "Which of the following is NOT a valid way to declare a variable in JavaScript?",
+    choices: ["var", "let", "const", "val"],
+    answer: "val"
+  },
+  {
+    question: "Which of the following is NOT a loop structure in JavaScript?",
+    choices: ["for", "while", "do...while", "repeat...until"],
+    answer: "repeat...until"
+  },
+  {
+    question: "What is the difference between '==' and '===' in JavaScript?",
+    choices: ["'==' performs a type conversion, while '===' does not", "'===' performs a type conversion, while '==' does not", "They are equivalent operators with no difference in functionality", "'==' and '===' cannot be used interchangeably"],
+    answer: "'==' performs a type conversion, while '===' does not"
+  },
+  {
+    question: "What is the output of the following code?\n\nvar x = 5;\nconsole.log(x++);\nconsole.log(x);",
+    choices: ["5, 6", "5, 5", "6, 6", "6, 5"],
+    answer: "5, 6"
+  },
+  {
+    question: "Which of the following is a method that can be used to add an element to the end of an array in JavaScript?",
+    choices: ["push()", "pop()", "shift()", "unshift()"],
+    answer: "push()"
+  },
+  {
+    question: "What is the result of the expression typeof(null) in JavaScript?",
+    choices: ["null", "undefined", "object", "number"],
+    answer: "object"
+  }
+];
+export { questions };
