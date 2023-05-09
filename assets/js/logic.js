9d2fecfa54ff967f9c067a63651f14e8150f6b87
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-05-09 01:57:55 +0100

    import statement corrected

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,4 @@
-import { questions } from 'questions.js';
+import { questions } from './questions.js';
 const startBtn = document.getElementById("start");
 const questionContainer = document.getElementById("questions");
 const questionEl = document.getElementById("question");
